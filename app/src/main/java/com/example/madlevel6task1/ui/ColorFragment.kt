47380b6fdda05f74356340cc371c8c004f58fd18
Acdaling <acdaling@gmail.com>
2020-11-15 14:48:23 +0100

    Initialized the recyclerview

--- app/src/main/java/com/example/madlevel6task1/ui/ColorFragment.kt
+++ app/src/main/java/com/example/madlevel6task1/ui/ColorFragment.kt
@@ -1,22 +1,28 @@
 package com.example.madlevel6task1.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel6task1.R
+import com.example.madlevel6task1.models.ColorItem
+import com.example.madlevel6task1.ui.adapters.ColorAdapter
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_color.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ColorFragment : Fragment() {
+    private val colors = arrayListOf<ColorItem>()
+    private lateinit var colorAdapter: ColorAdapter
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color, container, false)
@@ -25,8 +31,12 @@ class ColorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        colorAdapter = ColorAdapter(colors, ::onColorClick)
+        rvColors.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        rvColors.adapter = colorAdapter
+    }
+
+    private fun onColorClick(colorItem: ColorItem) {
+        Snackbar.make(rvColors, "This color is: ${colorItem.name}", Snackbar.LENGTH_LONG).show()
     }
 }
