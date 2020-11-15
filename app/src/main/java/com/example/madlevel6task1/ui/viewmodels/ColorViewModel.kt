6b5554790bd98c1cf62fcb89fa0aabb071565120
Acdaling <acdaling@gmail.com>
2020-11-15 14:58:47 +0100

    Created a viewModel for the colors

---
+++ app/src/main/java/com/example/madlevel6task1/ui/viewmodels/ColorViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.madlevel6task1.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.madlevel6task1.models.ColorItem
+import com.example.madlevel6task1.repositories.ColorRepository
+
+class ColorViewModel : ViewModel() {
+    private val colorRepository = ColorRepository()
+
+    val colorItems: LiveData<List<ColorItem>> get() = _colorItems
+
+    private val _colorItems = MutableLiveData<List<ColorItem>>().apply {
+        value = colorRepository.getColorItems()
+    }
+}
